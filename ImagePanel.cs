706a778bac9919b30b737d56f8447ecb074d62e9
Nakata <kojinktganbaoru2012@gmail.com>
2017-08-19 12:36:51 +0900

    update

--- ImagePanel.cs
+++ ImagePanel.cs
@@ -10,58 +10,54 @@ using System.IO;
 /// </summary>
 public class ImagePanel : MonoBehaviour {
 
-	public Texture tex = null;
-	private bool isFinish = false;
+	[Tooltip("表示させる画像")]
+	public Texture texure = null;
+
+	[Tooltip("破壊前に見える板")]
+	public GameObject panel;
+
+	[Tooltip("破壊後の破片を纏めているオブジェクト")]
+	public GameObject debris;
+
+	private bool isFinish = false; //debug
 
 	void Start()
 	{
 		//エラー処理
-		if (tex == null)
+		if (texure == null)
 		{
 			Debug.LogError("Texture is null");
 			isFinish = true;
 			return;
 		}
-		
-		//設定
-		foreach (Transform child in this.transform)
+
+		debris.SetActive(false); //飛び散らないように非アクティブにしておく
+
+		//Texture設定
+		panel.GetComponent<Renderer>().material.mainTexture = texure;
+		foreach (Transform child in debris.transform)
 		{
-			var render = child.gameObject.GetComponent<Renderer>();
-			render.material.mainTexture = tex;
-
-			var rbody = child.gameObject.GetComponent<Rigidbody>();
-			rbody.constraints = RigidbodyConstraints.FreezeAll;
-			rbody.useGravity = false;
-
-			var colider = child.gameObject.GetComponent<BoxCollider>();
-			colider.enabled = false;
+			child.gameObject.GetComponent<Renderer>().material.mainTexture = texure; //破片すべてにTexture設定
 		} 
-
 	}
 
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.Space))
+		//debug スペースキーを押すと爆散する
+		if (Input.GetKeyDown(KeyCode.Space) && !isFinish)
+		{
 			crash();
+			isFinish = true;
+		}
 	}
 
 
 	public void crash()
 	{
-		if (isFinish)
-			return;
+		debris.SetActive(true); //破片を表示
+		debris.transform.parent = null; //Destroy(this.gameObject)に巻き込まれないように
+		Destroy(debris, 10.0f);
 
-		//拘束を解除していく
-		foreach (Transform child in this.transform)
-		{
-			var rbody = child.gameObject.GetComponent<Rigidbody>();
-			rbody.constraints = RigidbodyConstraints.None;
-			rbody.useGravity = true;
 
-			var colider = child.gameObject.GetComponent<BoxCollider>();
-			colider.enabled = true;
-		}
-
-		isFinish = true;
-
+		Destroy(this.gameObject);
 	}
  }
